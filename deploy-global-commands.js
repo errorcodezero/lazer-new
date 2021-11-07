7ab96baa4e14dcc47711f38ccb547a646a3b37be
yeetsquad747 <74121237+yeetsquad747@users.noreply.github.com>
2021-11-07 08:52:32 -0800

    Fixing typo

--- deploy-global-commands.js
+++ deploy-global-commands.js
@@ -16,6 +16,6 @@ for (const file of commandFiles) {
 const rest = new REST({ version: "9" }).setToken(token);
 
 rest
-  .put(Routes.applicationGuildCommands(client_id), { body: commands })
+  .put(Routes.applicationCommands(client_id), { body: commands })
   .then(() => console.log("Successfully registered application commands."))
   .catch(console.error);
