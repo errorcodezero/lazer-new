a475c52fe3e833b223cc7a7731a44755886a385d
yeetsquad747 <74121237+yeetsquad747@users.noreply.github.com>
2021-11-07 09:26:41 -0800

    Adding status to bot

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,11 @@
 const { greenBright } = require('chalk');
+const { status } = require('../config');
 
 module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
 		console.log(greenBright(`Ready! Logged in as ${client.user.tag}`));
+    client.user.setActivity(status, { type: "PLAYING" });
 	},
 };
