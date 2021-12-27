15c234941277e18cdadea6d8d4ef5d584f1b0c0b
yeetsquad747 <74121237+yeetsquad747@users.noreply.github.com>
2021-12-27 09:38:26 -0800

    Update Shop.js

--- models/Shop.js
+++ models/Shop.js
@@ -2,7 +2,8 @@ const { Schema, model } = require("mongoose");
 
 const Shop = Schema({
   name: String,
-  cost: Int,
+  description: String,
+  cost: Number,
 });
 
 module.exports = model("Shop", Shop);
