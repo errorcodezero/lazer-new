e883f9cea9684626c4bcec262d199968f52b4fcd
yeetsquad747 <74121237+yeetsquad747@users.noreply.github.com>
2022-01-09 11:34:26 -0800

    Adding clean meme selector and random embed color

--- commands/meme.js
+++ commands/meme.js
@@ -1,23 +1,44 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const fetch = require("node-fetch");
 const { MessageEmbed } = require("discord.js");
+const randomHex = require('random-hex');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("meme")
-    .setDescription("Sends epic memes"),
-  cooldown: 1,
+    .setDescription("Sends epic memes")
+    .addBooleanOption((option) =>
+      option
+        .setName("clean")
+        .setDescription("Should the memes be clean")
+    ),
+  cooldown: 2,
   async execute(interaction) {
-    const data = await fetch("http://meme-api.herokuapp.com/gimme/memes").then(
-      (res) => res.json()
-    );
-    const embed = new MessageEmbed()
-      .setTitle(data.title)
-      .setURL(data.postLink)
-      .setImage(data.url)
-      .setFooter(data.ups + " upvotes")
-      .setTimestamp()
-      .setColor("#EC123E");
-    await interaction.reply({ embeds: [embed] });
+    const isClean = interaction.options.getBoolean("clean");
+    if(!isClean || isClean == false) {
+      const data = await fetch("http://meme-api.herokuapp.com/gimme/memes").then(
+        (res) => res.json()
+      );
+      const embed = new MessageEmbed()
+        .setTitle(data.title)
+        .setURL(data.postLink)
+        .setImage(data.url)
+        .setFooter({ text: `${data.ups} upvotes` })
+        .setTimestamp()
+        .setColor(randomHex.generate());
+      await interaction.reply({ embeds: [embed] });
+    } else {
+      const cleanData = await fetch("http://meme-api.herokuapp.com/gimme/cleanmemes").then(
+        (res) => res.json()
+      );
+      const embed = new MessageEmbed()
+        .setTitle(cleanData.title)
+        .setURL(cleanData.postLink)
+        .setImage(cleanData.url)
+        .setFooter({ text: `${cleanData.ups} upvotes` })
+        .setTimestamp()
+        .setColor(randomHex.generate());
+      await interaction.reply({ embeds: [embed] });
+    }
   },
 };
--- config.js
+++ config.js
@@ -3,6 +3,7 @@ module.exports = {
   token: process.env.BOT_TOKEN || "",
   status: process.env.BOT_STATUS || "",
   testing_guild_id: process.env.TESTING_GUILD_ID || "",
+  client_id: process.env.BOT_CLIENT_ID || "",
   mongo_db_url: process.env.MONGO_DB_URL || "",
   top_gg_token: process.env.TOP_GG_TOKEN || "",
 };
